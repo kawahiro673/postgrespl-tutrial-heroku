fe584b4c5ab0881fcda56c83ca903d6944f8982d
github hiro-kawaguchi673 <kmcnt673.9099@gmail.com>
2023-03-03 23:55:13 +0900

    Get query write

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 
 app.use(express.json());
 app.get('/', (req, res) => {
-  res.send('Hello Express');
   pool.query('SELECT * FROM users', (error, results) => {
     if (error) throw error;
     return res.status(200).json(results.rows);
   });
+  res.send('Hello Express');
 });
 
 //ユーザー情報を全て取得する
